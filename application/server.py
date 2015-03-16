25bab1bb2a7a739811785ff7111d36c4ee6e2121
treacle74 <ptrelease@gmail.com>
2015-03-16 10:41:22 +0000

    Changed so that outgoing queue can exist on a seprate ip address to the incoming queue

--- application/server.py
+++ application/server.py
@@ -30,7 +30,8 @@ app.config.from_object(os.environ.get('SETTINGS'))
 # Routing key is same as queue name in "default direct exchange" case; exchange name is blank.
 INCOMING_QUEUE = app.config['INCOMING_QUEUE']
 OUTGOING_QUEUE = app.config['OUTGOING_QUEUE']
-RP_HOSTNAME = app.config['RP_HOSTNAME']
+INCOMING_QUEUE_HOSTNAME = app.config['INCOMING_QUEUE_HOSTNAME']
+OUTGOING_QUEUE_HOSTNAME = app.config['OUTGOING_QUEUE_HOSTNAME']
 incoming_exchange = kombu.Exchange(type="direct", durable=True)
 outgoing_exchange = kombu.Exchange(type="fanout")
 
@@ -59,19 +60,19 @@ def setup_logger(name=__name__):
 logger = setup_logger()
 
 # RabbitMQ connection/channel; default user/password.
-def setup_connection(exchange=None):
+def setup_connection(queue_hostname, exchange=None):
     """ Attempt connection, with timeout. """
 
     # Attempt connection in a separate thread, as (implied) 'connect' call hangs if permissions not set etc.
     with stopit.ThreadingTimeout(10) as to_ctx_mgr:
         assert to_ctx_mgr.state == to_ctx_mgr.EXECUTING
 
-        connection = kombu.Connection(hostname=RP_HOSTNAME)
-        app.logger.info(RP_HOSTNAME)
+        connection = kombu.Connection(hostname=queue_hostname)
+        app.logger.info(queue_hostname)
         connection.connect()
 
     if to_ctx_mgr.state == to_ctx_mgr.TIMED_OUT:
-        err_msg = "Connection unavailable: {}".format(RP_HOSTNAME)
+        err_msg = "Connection unavailable: {}".format(queue_hostname)
         raise RuntimeError(err_msg)
 
 
@@ -88,7 +89,7 @@ def setup_connection(exchange=None):
 # Producer, for 'outgoing' exchange by default.
 def setup_producer(connection=None, exchange=outgoing_exchange, serializer='json'):
 
-    channel = setup_connection(exchange) if connection is None else connection
+    channel = setup_connection(app.config['OUTGOING_QUEUE_HOSTNAME'], exchange) if connection is None else connection
     logger.info("channel: {}".format(channel))
     logger.info("exchange: {}".format(exchange))
 
@@ -103,7 +104,7 @@ def setup_producer(connection=None, exchange=outgoing_exchange, serializer='json
 def setup_consumer(connection=None, exchange=incoming_exchange, queue_name=INCOMING_QUEUE, callback=None):
     """ Create consumer with single queue and callback """
 
-    channel = setup_connection(exchange) if connection is None else connection
+    channel = setup_connection(app.config['INCOMING_QUEUE_HOSTNAME'] ,exchange) if connection is None else connection
     logger.info("channel: {}".format(channel))
     logger.info("exchange: {}".format(exchange))
     logger.info("queue: {}".format(queue_name))
--- environment-test.sh
+++ environment-test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 export SETTINGS="config.PreviewConfig"
-export RP_HOSTNAME="amqp://guest:guest@localhost:5672//"
+export INCOMING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
+export OUTGOING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
 export INCOMING_QUEUE="INCOMING_QUEUE"
+export OUTGOING_QUEUE="OUTGOING_QUEUE"
 export COVERAGE_PROCESS_START="c:\\Users\\User\\register-publisher\\.coveragerc"
 
 
