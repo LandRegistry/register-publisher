ba8b43484f01603c6ee1be6b00af4787ff27ae53
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2015-03-19 11:04:36 +0000

    Modify test environment settings

--- environment-test.sh
+++ environment-test.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-export SETTINGS="config.PreviewConfig"
-export INCOMING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
-export OUTGOING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
-export INCOMING_QUEUE="INCOMING_QUEUE"
-export OUTGOING_QUEUE="OUTGOING_QUEUE"
+export SETTINGS="config.DevelopmentConfig"
 export COVERAGE_PROCESS_START="c:\\Users\\User\\register-publisher\\.coveragerc"
 
 
