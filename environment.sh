7cec1cc965104f67d7de8d385baa73ad4aedea4c
RichardProsser <ebizbytes@gmail.com>
2015-03-26 10:56:46 +0000

    Restored conventional SETTINGS default value ("config.DevelopmentConfig").

--- environment.sh
+++ environment.sh
@@ -2,7 +2,7 @@
 
 # Receive messages from "System Of Record" via default 'direct' exchange.
 # Publish to temporary queues - as specified by (unknown) clients - via default 'fanout' exchange.
-export SETTINGS="config.ProductionConfig"
+export SETTINGS="config.DevelopmentConfig"
 export INCOMING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
 export INCOMING_QUEUE="system_of_record"
 export OUTGOING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
