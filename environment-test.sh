f18b9de9c244ccba438aeb0418e74fe1b1a622cf
Skablam <stainlesssteelmatt@gmail.com>
2015-03-27 18:18:32 +0000

    only set environment variables for unit tests if they have not already been set

--- environment-test.sh
+++ environment-test.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 export SETTINGS="config.TestConfig"
-export INCOMING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
-export OUTGOING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
-export INCOMING_QUEUE="INCOMING_QUEUE"
-export OUTGOING_QUEUE="OUTGOING_QUEUE"
+
+if [ -z "${INCOMING_QUEUE_HOSTNAME}" ]; then
+  export INCOMING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
+fi
+
+if [ -z "${OUTGOING_QUEUE_HOSTNAME}" ]; then
+  export OUTGOING_QUEUE_HOSTNAME="amqp://guest:guest@localhost:5672//"
+fi
+
+if [ -z "${INCOMING_QUEUE}" ]; then
+  export INCOMING_QUEUE="INCOMING_QUEUE"
+fi
+
+if [ -z "${OUTGOING_QUEUE}" ]; then
+  export OUTGOING_QUEUE="OUTGOING_QUEUE"
+fi
