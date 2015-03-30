bbc02c000b7698a0ef2b824294975c791573466d
treacle74 <ptrelease@gmail.com>
2015-03-30 15:22:29 +0100

    proof of header concept

--- demo/complete_send.py
+++ demo/complete_send.py
@@ -25,6 +25,7 @@ with connection as conn:
     #: Publish the message using the json serializer (which is the default),
     #: and zlib compression. The kombu consumer will automatically detect
     #: encoding, serialization and compression used and decode accordingly.
-    producer.publish({'hello': 'world'}, exchange=exchange, routing_key=queue.routing_key,  serializer='json')
+    producer.publish({'hello': 'world'}, exchange=exchange, routing_key=queue.routing_key, serializer='json',
+                     headers={'title_number': 'DN1'})
 
 
