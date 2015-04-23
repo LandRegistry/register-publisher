cc390d10c8bc80e2d422f56577c6678343f9c710
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2015-04-23 11:56:15 +0100

    Force install of <2.0.0 of the apt module because puppet

--- provision.sh
+++ provision.sh
@@ -9,6 +9,8 @@ source /etc/profile.d/local_bin.sh
 
 gem install --no-ri --no-rdoc puppet:3.7.5
 
+puppet module install puppetlabs-apt --version "<2.0.0"
+
 puppet module install garethr-erlang
 
 puppet apply /home/vagrant/srv/register-publisher/manifests/erlang.pp
